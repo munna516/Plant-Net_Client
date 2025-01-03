ae76d7eb82eb2d29ff15954d6e332dfb5c8e1f11
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-04 01:34:59 +0600

    plant add and show done

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,51 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUpload } from "../../../Api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [uploadButtonText, setUploadButttonText] = useState("Upload");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageUrl = await imageUpload(image);
+    // seller info
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    // Create plant data object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageUrl,
+      seller,
+    };
+
+    // console.table(plantData);
+    try {
+      await axiosSecure.post("/plants", plantData);
+      toast.success("Data Added Successfully!");
+    } catch {
+      toast.error("Something Went Wrong");
+    }
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +53,13 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadButtonText={uploadButtonText}
+        setUploadButttonText={setUploadButttonText}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
